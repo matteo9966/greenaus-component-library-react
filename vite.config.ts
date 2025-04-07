e97b7a545cd6dd7a155cfa2f374fd4115828684c
Matteo Luigi Tommasi <m.tommasi@greenaus.it>
2025-04-07 12:17:43 +0200

    refactor: update build process and add TypeScript configuration for library
    - Changed build script in package.json to use specific tsconfig for building.
    - Added new tsconfig-build.json to include the 'lib' directory.
    - Updated tsconfig.app.json to include 'lib' in the compilation.
    - Enhanced vite.config.ts to support library build with dts plugin and specified entry point.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({ include: ["lib"], tsconfigPath: "./tsconfig.app.json" }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+    copyPublicDir: false,
+  },
+});
