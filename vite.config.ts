fa06dd82a504168daaa7ee75d69ce3f7cc62a752
Matteo Luigi Tommasi <m.tommasi@greenaus.it>
2025-05-06 19:43:21 +0200

    feat: add TypeScript support for CSS modules and update build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 import dts from "vite-plugin-dts";
-
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+import { extname, relative, resolve } from "path";
+import { fileURLToPath } from "url";
+import { glob } from "glob";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    libInjectCss(),
     dts({ include: ["lib"], tsconfigPath: "./tsconfig.app.json" }),
   ],
   build: {
@@ -17,6 +20,24 @@ export default defineConfig({
     copyPublicDir: false,
     rollupOptions: {
       external: ["react", "react-dom", "react/jsx-runtime"],
+      input: Object.fromEntries(
+        glob
+          .sync("lib/**/*.{ts,tsx}", {
+            ignore: ["**/*.d.ts", "**/*.test.ts", "**/*.test.tsx"],
+          })
+          .map((file) => [
+            // The name of the entry point
+            // lib/nested/foo.ts becomes nested/foo
+            relative("lib", file.slice(0, file.length - extname(file).length)),
+            // The absolute path to the entry file
+            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
     },
   },
 });
