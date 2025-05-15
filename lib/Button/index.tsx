3af8d6187e4f8f6486d8213278cf709e0209bd35
Matteo Luigi Tommasi <matteo_tommasi96@hotmail.com>
2025-05-15 20:00:54 +0200

    feat: update Button component with Bootstrap Italia styles and add card layout

--- lib/Button/index.tsx
+++ lib/Button/index.tsx
@@ -1,12 +1,24 @@
 import clsx from "clsx";
-import styles from "./styles.module.css";
+import "./styles.scss";
 export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   const { className, ...rest } = props;
   return (
-    <button
-      {...rest}
-      style={{ border: "1px solid green" }}
-      className={clsx(className, styles.button)}
-    />
+    <div className="greenaus-component-library-react">
+      <button
+        {...rest}
+        style={{ border: "1px solid green" }}
+        className={clsx("btn btn-outline-success")}
+      />
+
+<div className="card-wrapper border shadow">
+      <div className="card">
+        <div className="card-body">
+          <h3 className="card-title h5 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor…</h3>
+          <p className="card-text font-serif">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+    </div>
+   
+    </div>
   );
 }
